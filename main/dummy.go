158384dcf2b359795d27591aa7db8e71b9feacb9
Shutefuan <62222528+Shutefuan@users.noreply.github.com>
2026-01-31 17:23:33 +0100

    Dummy
    Dummy

---
+++ main/dummy.go
@@ -0,0 +1,16 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"logo_256\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/preview.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.1
+    y: 0.1
+    z: 0.1
+  }
+}
